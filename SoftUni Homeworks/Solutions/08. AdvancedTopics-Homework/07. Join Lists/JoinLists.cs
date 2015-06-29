19ff39c55afce4ea18fc2610124d6f9d7c45287f
A. Prodanow <prodanow21@gmail.com>
2015-06-29 15:17:39 +0300

    Add new homework in SoftUni Homeworks

---
+++ SoftUni Homeworks/Solutions/08. AdvancedTopics-Homework/07. Join Lists/JoinLists.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class JoinLists
+{
+    static void Main()
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        List<int> firstList = new List<int>();
+        foreach (var num in input)
+        {
+            firstList.Add(num);
+        }
+        input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        List<int> secondList = new List<int>();
+        foreach (var num in input)
+        {
+            secondList.Add(num);
+        }
+
+        List<int> result = firstList.ToList();
+        result.AddRange(secondList);
+        result = result.Distinct().ToList();
+        result.Sort();
+        foreach (var numList in result)
+        {
+            Console.Write(numList + " ");
+        }
+    }
+}
---
+++ SoftUni Homeworks/Solutions/08. AdvancedTopics-Homework/09. Extract URLs from Text/ExtractURLs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class ExtractURLs
+{
+    private static void Main()
+    {
+        //Version 1 using LINQ
+        //string rawString = Console.ReadLine();
+        //var links = rawString.Split("\t\n ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+        //    .Where(s => s.StartsWith("http://") || s.StartsWith("www.") || s.StartsWith("https://"));
+        //foreach (string s in links)
+        //{
+        //    Console.WriteLine(s);
+        //}
+
+        //Version 2 using Regex                                                                                       // \b       -matches a word boundary (spaces, periods..etc)
+        Regex linkParser = new Regex(@"\b(?:https?://|www\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase); // (?:      -define the beginning of a group, the ?: specifies not to capture the data within this group.                                                                                        
+        string rawString = Console.ReadLine();                                                                        // https?://  - Match http or https (the '?' after the "s" makes it optional)
+        foreach (Match m in linkParser.Matches(rawString))                                                            // |        -OR
+        {                                                                                                             // www\.    -literal string, match www. (the \. means a literal ".")
+            Console.WriteLine(m.Value);                                                                               // )        -end group
+        }                                                                                                             // \S+      -match a series of non-whitespace characters.
+    }                                                                                                                 // \b       -match the closing word boundary.
+}
